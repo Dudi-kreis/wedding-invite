325d763fd9b8936670c5f418b62fc08c6df95048
Dudi Kreis <dudikreis.com>
2026-03-29 01:01:41 +0300

    emvelop

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Wedding invite loaded");
+const envelope = document.getElementById("envelope");
+const invitation = document.getElementById("invitation");
+
+envelope.addEventListener("click", () => {
+    envelope.classList.add("open");
+
+    setTimeout(() => {
+        invitation.classList.remove("hidden");
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        });
+    }, 700);
+});
